3386ea9300cf1c7dfed6c1200021567e388080bd
Mark Ormesher <me@markormesher.co.uk>
2021-12-25 15:15:58 +0000

    overwrite old MQTT topics when there are few remaining departures

--- src/index.js
+++ src/index.js
@@ -32,7 +32,9 @@ mqttClient.on("error", (error) => logger.error("MQTT error", { error }));
 
 function mqttPublish(topic, message) {
   if (!mqttClient.connected) {
-    console.log("Attempted to publish MQTT message without a connection", { topic });
+    console.log("Attempted to publish MQTT message without a connection", {
+      topic,
+    });
   }
 
   mqttClient.publish(topic, message, {}, (error) => {
@@ -66,27 +68,89 @@ function updateDepartures() {
       const scheduledDepartureTime = departure["std"];
       const expectedDepartureTime = departure["etd"];
       const departureOnTime = expectedDepartureTime == "On time";
-      const departureTime = departureOnTime ? scheduledDepartureTime : expectedDepartureTime;
+      const departureTime = departureOnTime
+        ? scheduledDepartureTime
+        : expectedDepartureTime;
 
-      const arrivalStop = departure["subsequentCallingPoints"][departure["subsequentCallingPoints"].length - 1];
+      const arrivalStop =
+        departure["subsequentCallingPoints"][
+          departure["subsequentCallingPoints"].length - 1
+        ];
       const scheduledArrivalTime = arrivalStop["st"];
       const expectedArrivalTime = arrivalStop["et"];
       const arrivalOnTime = expectedArrivalTime == "On time";
-      const arrivalTime = arrivalOnTime ? scheduledArrivalTime : expectedArrivalTime;
+      const arrivalTime = arrivalOnTime
+        ? scheduledArrivalTime
+        : expectedArrivalTime;
 
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/departure_station`, LOCAL_STATION);
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/departure_time_scheduled`, scheduledDepartureTime);
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/departure_time_actual`, departureTime);
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/departure_on_time`, departureOnTime + "");
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/arrival_station`, destination);
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/arrival_time_scheduled`, scheduledArrivalTime);
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/arrival_time_actual`, arrivalTime);
-      mqttPublish(`${TOPIC_PREFIX}/journey_${rowNum}/arrival_on_time`, arrivalOnTime + "");
+      publishDeparture(
+        rowNum,
+        destination,
+        scheduledDepartureTime,
+        departureTime,
+        departureOnTime,
+        scheduledArrivalTime,
+        arrivalTime,
+        arrivalOnTime
+      );
+    }
+
+    // publish blank departures to "clean up" when fewer than ROWS_TO_PUBLISH remain
+    for (let i = rowNum + 1; i <= ROWS_TO_PUBLISH; ++i) {
+      publishDeparture(i);
     }
 
     mqttPublish(`${TOPIC_PREFIX}/last_seen`, new Date().toISOString());
     console.log(`Published journeys at ${new Date().toISOString()}`);
   });
+}
+
+function publishDeparture(
+  rowNum,
+  destination,
+  scheduledDepartureTime,
+  departureTime,
+  departureOnTime,
+  scheduledArrivalTime,
+  arrivalTime,
+  arrivalOnTime
+) {
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/departure_station`,
+    LOCAL_STATION
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/departure_time_scheduled`,
+    scheduledDepartureTime || ""
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/departure_time_actual`,
+    departureTime || ""
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/departure_on_time`,
+    departureOnTime === undefined || departureOnTime === null
+      ? ""
+      : departureOnTime + ""
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/arrival_station`,
+    destination || ""
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/arrival_time_scheduled`,
+    scheduledArrivalTime || ""
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/arrival_time_actual`,
+    arrivalTime || ""
+  );
+  mqttPublish(
+    `${TOPIC_PREFIX}/journey_${rowNum}/arrival_on_time`,
+    arrivalOnTime === undefined || arrivalOnTime === null
+      ? ""
+      : arrivalOnTime + ""
+  );
 }
 
 updateDepartures();
